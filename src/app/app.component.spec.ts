ad9f941f45431fe23504a0b023a369b99e8a6903
nas <nasreddine.skandrani@gmail.com>
2018-05-12 17:35:30 -0400

    Try **jest in parallel to karma

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
+// app
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
---
+++ src/setupJest.ts
@@ -0,0 +1,5 @@
+
+// adding jest https://www.xfive.co/blog/testing-angular-faster-jest/
+
+import 'jest-preset-angular';
+import './jestGlobalMocks';
