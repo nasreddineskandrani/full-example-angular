b07183955c6f60feb241043ff682cd840aaae8c0
unknown <nasreddine.skandrani@gmail.com>
2018-12-08 12:11:34 -0500

    toto

--- src/app/game/game-a.service.spec.ts
+++ src/app/game/game-a.service.spec.ts
@@ -4,21 +4,36 @@ import { Hero } from './hero.model';
 
 describe('game-a.service', () => {
   let gameAService: GameAService;
+  let heroService: HeroService;
+  let spy: jest.SpyInstance;
+
+  const heroService2 = new HeroService();
+  const spy2 = jest.spyOn(heroService2, 'computeMaxLevel');
 
   beforeEach(() => {
-    const heroService = new HeroService();
+    heroService = new HeroService();
+    spy = jest.spyOn(heroService, 'computeMaxLevel');
     gameAService = new GameAService(heroService);
   });
 
-  describe('play()', () => {
-    it('should be able to play', () => {
-      const firstHero: Hero = {
-        level: 44,
-        fly: false,
-        swim: false
-      };
-      const heroes = [firstHero];
-      expect(gameAService.play(heroes)).toEqual(true);
-    });
+  it('test for PR infer type jest', () => {
+    spy.mockReturnValue('2');
+    spy.mockReturnValue(2);
+
+    expect(1).toBe(1);
+  });
+
+  it('test for PR infer type jest', () => {
+    jest.spyOn(heroService, 'computeMaxLevel').mockReturnValue('2');
+    jest.spyOn(heroService, 'computeMaxLevel').mockReturnValue(2);
+
+    expect(1).toBe(1);
+  });
+
+  it('test for PR infer type jest', () => {
+    spy2.mockReturnValue('2');
+    spy2.mockReturnValue(2);
+
+    expect(1).toBe(1);
   });
 });
